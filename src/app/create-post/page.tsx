2f022970d772e5d0018a33ba9349e63dc4dd05c9
nikunj309 <103521892+nikunj309@users.noreply.github.com>
2024-05-21 22:44:57 +0530

    change font file

--- src/app/create-post/page.tsx
+++ src/app/create-post/page.tsx
@@ -1,90 +1,90 @@
-"use client"
-import { CldUploadButton, CloudinaryUploadWidgetResults } from 'next-cloudinary'
-import React, { useState } from 'react'
+// "use client"
+// import { CldUploadButton, CloudinaryUploadWidgetResults } from 'next-cloudinary'
+// import React, { useState } from 'react'
 
-const page = () => {
-    const [slug, setSlug] = useState('');
-    const [title, setTitle] = useState('');
-    const [desc, setDesc] = useState('');
-    const [image1, setImage1] = useState('');
-    const [image2, setImage2] = useState('');
-    const [publicId, setPublicId] = useState("");
-    const [publicId2, setPublicId2] = useState("");
+// const page = () => {
+//     const [slug, setSlug] = useState('');
+//     const [title, setTitle] = useState('');
+//     const [desc, setDesc] = useState('');
+//     const [image1, setImage1] = useState('');
+//     const [image2, setImage2] = useState('');
+//     const [publicId, setPublicId] = useState("");
+//     const [publicId2, setPublicId2] = useState("");
 
-    const handleImageUpload1 = (result: CloudinaryUploadWidgetResults) => {
-        console.log("result: ", result);
-        const info = result.info as object;
+//     const handleImageUpload1 = (result: CloudinaryUploadWidgetResults) => {
+//         console.log("result: ", result);
+//         const info = result.info as object;
 
-        if ("secure_url" in info && "public_id" in info) {
-            const url = info.secure_url as string;
-            const public_id = info.public_id as string;
-            setImage1(url);
-            setPublicId(public_id);
-            console.log("url: ", url);
-            console.log("public_id: ", public_id);
-        }
-    };
-    const handleImageUpload2 = (result: CloudinaryUploadWidgetResults) => {
-        console.log("result: ", result);
-        const info = result.info as object;
+//         if ("secure_url" in info && "public_id" in info) {
+//             const url = info.secure_url as string;
+//             const public_id = info.public_id as string;
+//             setImage1(url);
+//             setPublicId(public_id);
+//             console.log("url: ", url);
+//             console.log("public_id: ", public_id);
+//         }
+//     };
+//     const handleImageUpload2 = (result: CloudinaryUploadWidgetResults) => {
+//         console.log("result: ", result);
+//         const info = result.info as object;
 
-        if ("secure_url" in info && "public_id" in info) {
-            const url = info.secure_url as string;
-            const public_id = info.public_id as string;
-            setImage2(url);
-            setPublicId2(public_id);
-            console.log("url: ", url);
-            console.log("public_id: ", public_id);
-        }
-    };
+//         if ("secure_url" in info && "public_id" in info) {
+//             const url = info.secure_url as string;
+//             const public_id = info.public_id as string;
+//             setImage2(url);
+//             setPublicId2(public_id);
+//             console.log("url: ", url);
+//             console.log("public_id: ", public_id);
+//         }
+//     };
 
 
-    const handleSubmit = () => {
+//     const handleSubmit = () => {
 
-    }
-    return (
-        <div>
-            <h1>Create a New Post</h1>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Slug:
-                    <input type="text" value={slug} onChange={(e) => setSlug(e.target.value)} />
-                </label>
-                <br />
-                <label>
-                    Title:
-                    <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-                </label>
-                <br />
-                <label>
-                    Description:
-                    <textarea value={desc} onChange={(e) => setDesc(e.target.value)} />
-                </label>
-                <br />
-                <label>
-                    Image 1:
-                    <CldUploadButton
-                        uploadPreset="owejpqbm"
-                        onSuccess={handleImageUpload1}
-                    >
-                        Upload Image 1
-                    </CldUploadButton>
-                </label>
-                <br />
-                <label>
-                    Image 2:
-                    <CldUploadButton
-                        uploadPreset="owejpqbm"
-                        onSuccess={handleImageUpload2}
-                    >
-                        Upload Image 2
-                    </CldUploadButton>
-                </label>
-                <br />
-                <button type="submit">Create Post</button>
-            </form>
-        </div>
-    )
-}
+//     }
+//     return (
+//         <div>
+//             <h1>Create a New Post</h1>
+//             <form onSubmit={handleSubmit}>
+//                 <label>
+//                     Slug:
+//                     <input type="text" value={slug} onChange={(e) => setSlug(e.target.value)} />
+//                 </label>
+//                 <br />
+//                 <label>
+//                     Title:
+//                     <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+//                 </label>
+//                 <br />
+//                 <label>
+//                     Description:
+//                     <textarea value={desc} onChange={(e) => setDesc(e.target.value)} />
+//                 </label>
+//                 <br />
+//                 <label>
+//                     Image 1:
+//                     <CldUploadButton
+//                         uploadPreset="owejpqbm"
+//                         onSuccess={handleImageUpload1}
+//                     >
+//                         Upload Image 1
+//                     </CldUploadButton>
+//                 </label>
+//                 <br />
+//                 <label>
+//                     Image 2:
+//                     <CldUploadButton
+//                         uploadPreset="owejpqbm"
+//                         onSuccess={handleImageUpload2}
+//                     >
+//                         Upload Image 2
+//                     </CldUploadButton>
+//                 </label>
+//                 <br />
+//                 <button type="submit">Create Post</button>
+//             </form>
+//         </div>
+//     )
+// }
 
-export default page
+// export default page
